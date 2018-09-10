704162546adb7206d7f0d7bd0bc2031316cec64b
yyIntsuki <intsuki.yz@gmail.com>
2018-09-10 12:16:45 +0200

    Added .gitignore.

---
+++ DU/DU.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DU.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket program/uppgift vill du köra?");
+            int inputPr = Convert.ToInt32(Console.ReadLine());
+
+            switch (inputPr)
+            {
+                case 1:
+                    Console.WriteLine("Skriv ut antal ord:");
+                    break;
+
+                case 2:
+                    break;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
